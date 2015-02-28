b342a9cb5070b88d8e5536a1e1143487e16da2b9
yutarouk <1ds10175n@gmail.com>
2015-02-28 16:03:42 +0900

    bullFightingのまま

---
+++ ImageCaptureSample/RecViewController.m
@@ -0,0 +1,57 @@
+//
+//  RecViewController.m
+//  ImageCaptureSample
+//
+//  Copyright (c) 2014 Olympus Imaging Corporation. All rights reserved.
+//
+
+#import "RecViewController.h"
+
+@interface RecViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation RecViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (void)dealloc
+{
+	self.image = nil;
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+	return YES;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+	[super viewWillAppear:animated];
+	[self.navigationController setNavigationBarHidden:YES animated:animated];
+    self.imageView.image = self.image;
+}
+- (void)viewDidAppear:(BOOL)animated
+{
+    [self performSegueWithIdentifier:@"backToLiveView" sender:self];
+}
+- (IBAction)viewDidTap:(id)sender
+{
+	[self performSegueWithIdentifier:@"backToLiveView" sender:self];
+}
+
+- (IBAction)camreaButtonDidTap:(id)sender {
+	[self performSegueWithIdentifier:@"backToLiveView" sender:self];
+}
+
+@end
